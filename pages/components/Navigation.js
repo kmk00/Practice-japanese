3092352c791a1fa8244da73f30bdc34328078172
kmk00 <73724318+kmk00@users.noreply.github.com>
2023-02-04 17:50:37 +0100

    Add animations to links

--- pages/components/Navigation.js
+++ pages/components/Navigation.js
@@ -25,39 +25,60 @@ export const Navigation = () => {
       {navigate && (
         <>
           <div className="fixed bg-black/80 top-0 left-0 w-screen h-screen z-0"></div>
-          <div className="fixed left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] bg-[#fff0de] z-50 p-4 md:p-8 text-center text-md md:text-4xl font-black tracking-widest flex flex-col gap-8 rounded-xl shadow-black shadow-lg">
+          <div className="fixed left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] bg-[#fff0de] z-50 p-4 md:p-8 text-center text-md md:text-4xl font-black tracking-widest flex flex-col items-center gap-8 rounded-xl shadow-black shadow-lg">
             <p className="text-xl md:text-6xl underline ">Select mode</p>
-            <div onClick={showNavigation}>
+            <div
+              className="hover:border-b-4 border-black duration-75 w-fit ease-in"
+              onClick={showNavigation}
+            >
               <Link href="/">
                 <p>Draw symbol</p>
               </Link>
             </div>
-            <div onClick={showNavigation}>
+            <div
+              className="hover:border-b-4 border-black duration-75 w-fit ease-in"
+              onClick={showNavigation}
+            >
               <Link href="/">
                 <p>Guess the symbol</p>
               </Link>
             </div>
-            <div onClick={showNavigation}>
+            <div
+              className="hover:border-b-4 border-black duration-100 ease-in"
+              onClick={showNavigation}
+            >
               <Link href="/">
                 <p>Write Alphabet</p>
               </Link>
             </div>
-            <div onClick={showNavigation}>
+            <div
+              className="hover:border-b-4 border-black duration-75 w-fit ease-in"
+              onClick={showNavigation}
+            >
               <Link href="/">
                 <p>Practice Sound</p>
               </Link>
             </div>
-            <div onClick={showNavigation}>
+            <div
+              className="hover:border-b-4 border-black duration-75 w-fit ease-in"
+              onClick={showNavigation}
+            >
               <Link href="/">
                 <p>Words Translation</p>
               </Link>
             </div>
-            <div onClick={showNavigation}>
+            <div
+              className="hover:border-b-4 border-black duration-75 w-fit ease-in"
+              onClick={showNavigation}
+            >
               <Link href="/">
                 <p>External sources</p>
               </Link>
             </div>
-            <div className="mt-4" onClick={showNavigation}>
+            <div
+              className="hover:border-b-4 border-black duration-75 w-fit ease-in mt-4"
+              onClick={showNavigation}
+            >
               <Link href="/">
                 <p>Return</p>
               </Link>
