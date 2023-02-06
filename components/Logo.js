7ee4a9026291b347109c6e8945c41461c1abf625
kmk00 <73724318+kmk00@users.noreply.github.com>
2023-02-06 14:06:12 +0100

    Set logo

--- components/Logo.js
+++ components/Logo.js
@@ -1,8 +1,21 @@
 import React from "react";
+import Image from "next/image";
 
 export const Logo = () => {
   return (
-    <div id="topPage" className="xl:w-1/4 flex justify-center items-center p-4">
+    <div
+      id="topPage"
+      className=" flex justify-center gap-2 md:gap-4 items-center p-4"
+    >
+      <div className="relative xl:w-40 xl:h-40 md:w-28 md:h-28 w-16 h-16">
+        <Image
+          fill
+          className=""
+          src="/bonsai.svg"
+          alt="logo image"
+          priority
+        ></Image>
+      </div>
       <p className="text-2xl md:text-6xl font-black"> Learn Japanese</p>
     </div>
   );
