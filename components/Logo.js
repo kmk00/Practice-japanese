f08021c91823d5a92c83503de6dd2fdf3a50a4a3
kmk00 <73724318+kmk00@users.noreply.github.com>
2023-02-06 17:47:37 +0100

    Update Logo.js

--- components/Logo.js
+++ components/Logo.js
@@ -7,16 +7,12 @@ export const Logo = () => {
       id="topPage"
       className=" flex justify-center gap-2 md:gap-4 items-center p-4"
     >
-      <div className="relative xl:w-40 xl:h-40 md:w-28 md:h-28 w-16 h-16">
-        <Image
-          fill
-          className=""
-          src="/bonsai.svg"
-          alt="logo image"
-          priority
-        ></Image>
-      </div>
-      <p className="text-2xl md:text-6xl font-black"> Learn Japanese</p>
+      <Link className=" cursor-pointer" href="/">
+        <div className="relative xl:w-40 xl:h-40 md:w-28 md:h-28 w-16 h-16">
+          <Image fill src="/bonsai.svg" alt="logo image" priority></Image>
+        </div>
+        <p className="text-2xl md:text-6xl font-black"> Learn Japanese</p>
+      </Link>
     </div>
   );
 };
