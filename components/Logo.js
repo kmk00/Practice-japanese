ea6a973e22270ca5d5a7986ca80e11e319765d45
kmk00 <73724318+kmk00@users.noreply.github.com>
2023-02-06 18:10:39 +0100

    Update Logo.js

--- components/Logo.js
+++ components/Logo.js
@@ -9,10 +9,12 @@ export const Logo = () => {
       className=" flex justify-center gap-2 md:gap-4 items-center p-4"
     >
       <Link className=" cursor-pointer" href="/">
-        <div className="relative xl:w-40 xl:h-40 md:w-28 md:h-28 w-16 h-16">
-          <Image fill src="/bonsai.svg" alt="logo image" priority></Image>
+        <div className="flex gap-2">
+          <div className="relative xl:w-40 xl:h-40 md:w-28 md:h-28 w-16 h-16">
+            <Image fill src="/bonsai.svg" alt="logo image" priority></Image>
+          </div>
+          <p className="text-2xl md:text-6xl font-black"> Learn Japanese</p>
         </div>
-        <p className="text-2xl md:text-6xl font-black"> Learn Japanese</p>
       </Link>
     </div>
   );
